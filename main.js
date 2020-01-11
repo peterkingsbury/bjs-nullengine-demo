cc99df5e023cf3f6265719d4d9feb8968e5b39ef
Peter Kingsbury <peter.kingsbury@gmail.com>
2020-01-11 15:09:28 -0500

    BJS + Physics Engine Demo

---
+++ main.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const BABYLON = require('babylonjs');
+const LOADERS = require('babylonjs-loaders');
+const {
+  NullEngine,
+  Scene,
+  PointLight,
+  Vector3,
+  ArcRotateCamera,
+  OimoJSPlugin,
+  AmmoJSPlugin,
+  CannonJSPlugin,
+  SceneLoader: {
+    ImportMeshAsync
+  }
+} = BABYLON;
+
+global.XMLHttpRequest = require('xhr2').XMLHttpRequest;
+
+(async () => {
+  const engine = new NullEngine();
+  const scene = new Scene(engine);
+  const light = new PointLight("Omni", new Vector3(20, 20, 100), scene);
+  const camera = new ArcRotateCamera("Camera", 0, 0.8, 100, BABYLON.Vector3.Zero(), scene);
+  let result = null;
+  let meshes = null;
+  let physicsEngine = null;
+
+  try { physicsEngine = new OimoJSPlugin() } catch (e) { console.log(e) }
+  try { physicsEngine = new AmmoJSPlugin() } catch (e) { console.log(e) }
+  try { physicsEngine = new CannonJSPlugin() } catch (e) { console.log(e) }
+
+  if (physicsEngine) {
+    scene.enablePhysics(undefined, physicsEngine);
+  }
+
+  try {
+    result = await ImportMeshAsync("", "https://playground.babylonjs.com/scenes/", "skull.babylon", scene);
+    meshes = result.meshes;
+    camera.target = meshes[0];
+    console.log("Meshes loaded from babylon file: " + meshes.length);
+    for (const mesh of meshes) {
+      console.log(mesh.toString());
+    }
+  } catch (e) {
+    console.log(e);
+  }
+
+  try {
+    result = await ImportMeshAsync("", "https://www.babylonjs.com/Assets/DamagedHelmet/glTF/", "DamagedHelmet.gltf", scene);
+    meshes = result.meshes;
+    console.log("Meshes loaded from gltf file: " + meshes.length);
+    for (const mesh of meshes) {
+      console.log(mesh.toString());
+    }
+  } catch (e) {
+    console.log(e);
+  }
+
+  console.log("render started");
+  engine.runRenderLoop(scene.render.bind(scene));
+})();
+
